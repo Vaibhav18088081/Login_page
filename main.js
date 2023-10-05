36677b94ec4b46da7b34d0a8032ea7380c04b40a
Vaibhav18088081 <146832657+Vaibhav18088081@users.noreply.github.com>
2023-10-05 13:42:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const loginText = document.querySelector(".title-text .login");
-const loginForm = document.querySelector("form.login");
-const loginBtn = document.querySelector("label.login");
+
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
 signupBtn.onclick = (() => {
